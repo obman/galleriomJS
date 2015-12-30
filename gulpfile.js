c5c5ca9cf32cacdf909869fecf59ea7afcf7a78e
Denis Mušić <deniss9oo@gmail.com>
2015-12-30 17:11:12 +0100

    Modification on gulp minifying license comments

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp         = require('gulp'),
 	sourcemaps   = require('gulp-sourcemaps'),
 	autoprefixer = require('gulp-autoprefixer'),
 	cssmin       = require('gulp-cssmin'),
-	jsmin        = require('gulp-jsmin'),
+	uglify       = require('gulp-uglify'),
 	rename       = require('gulp-rename');
 
 
@@ -19,13 +19,23 @@ gulp.task('css', function() {
 
 gulp.task('js', function() {
     return gulp.src('galleriom.js')
-        .pipe(jsmin())
+        .pipe(uglify({ preserveComments: 'license' }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(''))
 });
 
+gulp.task('copy-css', function() {
+    return gulp.src('galleriom.min.css')
+        .pipe(gulp.dest('../gh-pages/'));
+});
+
+gulp.task('copy-js', function() {
+    return gulp.src('galleriom.min.js')
+        .pipe(gulp.dest('../gh-pages/'));
+});
+
 // Watcher for GalleriomJS app
 gulp.task('watch', function() {
-    gulp.watch('galleriom.css', ['css']);
-    gulp.watch('galleriom.js', ['js']);
+    gulp.watch('galleriom.css', ['css', 'copy-css']);
+    gulp.watch('galleriom.js', ['js', 'copy-js']);
 });
